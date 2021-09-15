1f11f062ce1cd8dc65c7801a3d055542b55e1b2d
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-09-15 14:36:55 +0100

    Added  App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/**  Add the import for react, changeEvent
+ *   The useState Method is the react hook that allows you to
+ *   use state in functional component.
+*/
+import React, { ChangeEvent, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+    // The note variables here represents the state and the setNote method represents 
+    //  the method for adding states
+    const [note, setNote] = useState("")
+    // The notes is an array and the setNotes is represents how the method for adding array
+    // Notice the syntax for useState method. 
+    
+    const [notes, setNotes] = useState<string[]>([])
+
+    const updateNote = (event: ChangeEvent<HTMLInputElement>) =>{
+            setNote(event.target.value)
+    }
+
+    function populateArray(){
+        setNotes([
+            ...notes,
+            note
+        ])
+        setNote("")
+    }
+
+    return(
+        <div>
+            <div className="input-section">
+                <input
+                    onChange={updateNote}
+                    value={note}
+                    type="text"
+                    name="note"
+                    placeholder="Note"
+                />
+                <button onClick={populateArray}>Add String</button>
+            </div>
+            <hr/>
+            <ul>
+                {
+                    notes.map((note) => 
+                            <li key={note} >{note}</li>
+                    )
+                }
+            </ul>
+            
+        </div>
+    )
 }
 
 export default App;
